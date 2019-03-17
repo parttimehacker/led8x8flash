c607ca55e3c0a85ae4bbc3ece2b2baf567bf7531
Dave Wilson <parttimehacker@gmail.com>
2019-03-17 07:29:56 -0700

    Updates to fix intial post
    Numerous fixes to initial post

--- examples/ex_led8x8flash.py
+++ examples/ex_led8x8flash.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """ Example of the Flash 8x8 LED matrix dispplay class """
 
-import threading
+from threading import Lock
 
 from Adafruit_Python_LED_Backpack.Adafruit_LED_Backpack import BicolorMatrix8x8
 
-import led8x8flash
+from led8x8flash.led8x8flash import Led8x8Flash
 
 if __name__ == '__main__':
-    LOCK = threading.Lock.Lock()
-    DISPLAY = BicolorMatrix8x8()
-    FLASH = led8x8flash.Led8x8Flash(MATRIX, LOCK, 2, 1.0)
+    LOCK = Lock()
+    DISPLAY = BicolorMatrix8x8.BicolorMatrix8x8()
+    FLASH = Led8x8Flash(DISPLAY, LOCK, 1, 0.5)
     FLASH.reset()
-    while True:
-    	FLASH.display()
-
+    for test in range(10):
+        FLASH.display()
+    FLASH.set_color(2)
+    for test in range(10):
+        FLASH.display()
+    FLASH.set_color(3)
+    for test in range(10):
+        FLASH.display()
--- led8x8flash.py
+++ led8x8flash.py
@@ -18,21 +18,21 @@ class Led8x8Flash:
         self.bus_lock = lock
         self.alternate = PING
         if color < 0:
-        	self.color = 0
-        	raise Exception('color should be greater than 0 The value of color was: {}'.format(x))
+            self.color = 0
+            raise Exception('color should be greater than 0 The value of color was: {}'.format(x))
         elif color > 3:
-        	self.color = 3
+            self.color = 3
             raise Exception('color should not be greater than 3 The value of color was: {}'.format(x))
         else:
-        	self.color = color
+            self.color = color
         if interval < 0.2:
             self.interval = 0.2
-        	raise Exception('interval should be greater than 0 The value of interval was: {}'.format(x))
+            raise Exception('interval should be greater than 0 The value of interval was: {}'.format(x))
         elif interval > 5.0:
             self.interval = 5.0
-        	raise Exception('interval should not be greater than 5 The value of interval was: {}'.format(x))
-        else
-        	self.interval = interval
+            raise Exception('interval should not be greater than 5 The value of interval was: {}'.format(x))
+        else:
+            self.interval = interval
         self.interval = interval
 
     def reset(self,):
@@ -42,20 +42,34 @@ class Led8x8Flash:
         self.matrix.set_brightness(BRIGHTNESS)
         self.bus_lock.release()
 
+    def set_color(self, color):
+        """ initialize to starting state and set brightness """
+        self.bus_lock.acquire(True)
+        if color < 0:
+            self.color = 0
+            raise Exception('color should be greater than 0 The value of color was: {}'.format(x))
+        elif color > 3:
+            self.color = 3
+            raise Exception('color should not be greater than 3 The value of color was: {}'.format(x))
+        else:
+            self.color = color
+        self.bus_lock.release()
+
     def display(self,):
         """ display the series as a 64 bit image with alternating colored pixels """
         time.sleep(self.interval)
         self.bus_lock.acquire(True)
-        if self.alternate == PING
-        	self.alternate = PONG
-        else
-        	self.alternate = PING
+        if self.alternate == PING:
+            self.alternate = PONG
+        else:
+            self.alternate = PING
         for xpixel in range(0, 8):
             for ypixel in range(0, 8):
-            	if self.alternate = PING:
-                	self.matrix.set_pixel(xpixel, ypixel, self.color
+                if self.alternate == PING:
+                    self.matrix.set_pixel(xpixel, ypixel, self.color)
                 else:
-                	self.matrix.set_pixel(xpixel, ypixel, 0)
+                    self.matrix.set_pixel(xpixel, ypixel, 0)
+        self.matrix.write_display()
         self.bus_lock.release()
 
 if __name__ == '__main__':
